22e7a2ed605d1282f1cf46fa1bfd00b8102fc35b
Asep Sopiyan <asep.sopiyan1309@gmail.com>
2021-11-09 10:26:55 +0700

    BASIC THEORY

--- PROJECT 1.py
+++ PROJECT 1.py
@@ -1,6 +1,25 @@
 '''
-Ini adalah demo project dengan python
+Ibu Menyuruh andi ke pasar membeli 1 botol susu, dan bilamana ada telur. beli 1 botol susu dan 6 butir telur
 '''
+print("andi pergi ke pasar")
+print("andi menanyakan apakah ada susu?")
 
-print('hello worl')
-print('my name is asep')
+stock_milk = 180
+stock_egg = 50
+
+if stock_milk > 0 :
+    print(f"stok susu ada : {stock_milk} botol")
+    print("andi menanyakan stock telur")
+
+    if stock_egg > 0 :
+        print(f"stok telur ada : {stock_egg} butir")
+        print("andi membeli 1 botol susu dan 6 butir telur")
+    else:
+        print("telur habis")
+        print("andi hanya membeli 1 botol susu")
+else:
+    print(f"susu habis")
+    print("andi tidak jadi membeli susu")
+
+print("andi pulang ke rumah")
+print("andi menyerahkan belanjaan ke ibu")
