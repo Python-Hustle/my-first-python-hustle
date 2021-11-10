c9504cbb46434df70c3383f6e7ea5712d8bd203d
Asep Sopiyan <asep.sopiyan1309@gmail.com>
2021-11-10 11:43:21 +0700

    PERULANGAN WHILE DENGAN HENTI

--- Syntaksis Perulangan Guru & Tugas.py
+++ Syntaksis Perulangan Guru & Tugas.py
@@ -1,12 +1,11 @@
 """
 Guru sedang mengecek tugas dari siswanya yang berjumlah 50 orang.
 """
-jumlah_tugas = 10
+jumlah_tugas = 20
 print(f"{jumlah_tugas} siswa telah mengumpulkan tugas")
 print("kemudian guru mulai mengecek")
 
 jumlah_tugas_sudah_dicek = 0
-
 for jumlah_tugas_sudah_dicek in range (1, jumlah_tugas+1):
     print(f"tugas ke-{jumlah_tugas_sudah_dicek} telah di cek")
 
